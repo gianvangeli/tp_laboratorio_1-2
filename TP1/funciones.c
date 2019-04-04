f71b83b4239b0d89045577a81127a608bc18877b
jsalerno85 <40808753+jsalerno85@users.noreply.github.com>
2019-04-04 17:47:58 -0300

    Add files via upload

---
+++ TP1/funciones.c
@@ -0,0 +1,50 @@
+float sumarOperandos(float x,float y) // Función que calcula la suma de dos números flotantes.
+{
+    float suma; // Declaración de la variable "suma", cuyo valor será la suma de de los valores "x" e "y".
+    suma=x+y; // Valor de la variable "suma".
+    printf("\nLa suma de %.2f y %.2f es igual a %.2f.\n",x,y,suma); // Mensaje donde se indica el resultado final, con solo dos decimales.
+    return 0; // Valor de retorno de la función.
+}
+
+float restarOperandos(float x,float y) // Función que calcula la resta de dos números flotantes.
+{
+    float resta; // Declaración de la variable "resta", cuyo valor será la resta de los valores "x" e "y".
+    resta=x-y; //Valor de la variable "resta".
+    printf("\nLa resta de %.2f y %.2f es igual a %.2f.\n",x,y,resta); // Mensaje donde se indica el resultado final, con solo dos decimales.
+    return 0; // Valor de retorno de la función.
+}
+
+float dividirOperandos(float x,float y) // Función que calcula el cociente de dos números flotantes.
+{
+    float cociente; // Declaración de la variable "cociente", cuyo valor será el cociente de los valores "x" e "y".
+    if(y==0) // Condición donde el valor de la variable "y" es igual a 0.
+    {
+        printf("\nLa division por el numero 0 no es posible\n"); // Sentencia que indica el error al dividir por 0.
+    }
+    else // Condición donde el valor de la variable "y" no es igual a 0.
+    {
+        cociente=x/y; // Valor de la variable "cociente".
+        printf("\nEl cociente de %.2f y %.2f es igual a %.2f.\n",x,y,cociente); // Mensaje donde se indica el resultado final, con solo dos decimales.
+        return 0; // Valor de retorno de la función.
+    }
+}
+
+float multiplicarOperandos(float x,float y) // Función que calcula la multiplicación de dos números flotantes.
+{
+    float multiplicacion; // Declaración de la variable "multiplicacion", cuyo valor será la multiplicacion de los valores "x" e "y".
+    multiplicacion=x*y; //Valor de la variable "multiplicacion".
+    printf("\nLa multiplicacion de %.2f por %.2f es igual a %.2f.\n",x,y,multiplicacion); // Mensaje donde se indica el resultado final, con solo dos decimales.
+    return 0; // Valor de retorno de la función.
+}
+
+int factorial(int x) // Función que calcula el factorial de un número entero.
+{
+    int i; // Declaración de la variable "i", que será utilizada para la cantidad de iteraciones y condiciones del for.
+    int aux=1; // Declaración y valor de la variable "aux", que se utilizará para guardar los valores de cada iteración del for.
+    for(i=1;i<x+1;i++) // Cantidad de iteraciones que se realizarán (desde i=1 hasta el valor de la variable "x").
+    {
+        aux=aux*i; // Valor que tomará la variable "aux" luego de cada iteración.
+    }
+    printf("\nEl factorial de %d es igual a %d.\n",x,aux); // Mensaje donde se indica el resultado final, con solo dos decimales.
+    return aux; // Valor de retorno de la función.
+}
---
+++ TP1/main.c
@@ -0,0 +1,62 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "funciones.h"
+
+int main()
+{
+    char seguir='s';
+    int opcion=0;
+    float A=0;
+    float B=0;
+
+    while(seguir=='s')
+    {
+        printf("\n1- Ingresar 1er operando (A=%.2f)\n",A);
+        printf("2- Ingresar 2do operando (B=%.2f)\n",B);
+        printf("3- Calcular la suma (A=%.2f+B=%.2f)\n",A,B);
+        printf("4- Calcular la resta (A=%.2f-B=%.2f)\n",A,B);
+        printf("5- Calcular la division (A=%.2f/B=%.2f)\n",A,B);
+        printf("6- Calcular la multiplicacion (A=%.2f*B=%.2f)\n",A,B);
+        printf("7- Calcular el factorial (A=%.2f)\n",A);
+        printf("8- Calcular todas las operaciones\n");
+        printf("9- Salir\n\n");
+        scanf("%d",&opcion);
+        switch(opcion)
+        {
+            case 1:
+                printf("\nIngrese primer operando: ");
+                scanf("%f",&A);
+                break;
+            case 2:
+                printf("\nIngrese segundo operando: ");
+                scanf("%f",&B);
+                break;
+            case 3:
+                sumarOperandos(A,B);
+                break;
+            case 4:
+                restarOperandos(A,B);
+                break;
+            case 5:
+                dividirOperandos(A,B);
+                break;
+            case 6:
+                multiplicarOperandos(A,B);
+                break;
+            case 7:
+                factorial(A);
+                break;
+            case 8:
+                sumarOperandos(A,B);
+                restarOperandos(A,B);
+                dividirOperandos(A,B);
+                multiplicarOperandos(A,B);
+                factorial(A);
+                break;
+            case 9:
+                seguir = 'n';
+                break;
+        }
+    }
+    return 0;
+}
